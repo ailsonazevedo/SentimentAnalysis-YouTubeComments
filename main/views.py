c8245a9eb3811e51a75a58bb560f46b8eb42e85d
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2022-06-25 23:06:00 -0300

    feat: add templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from sentimentanalysis_npl import sentimentanalysis_npl
 
 # Create your views here.
+def home(request):
+    return render(request,'index.html')
